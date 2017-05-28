90e833e3c18fcaac969da57bc458e4f78a251e4f
Carl Zulauf <carl@linkleaf.com>
2017-05-28 01:09:28 -0500

    Small cleanup of benchmark

--- benchmark.rb
+++ benchmark.rb
@@ -8,71 +8,58 @@ lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "opt_struct"
 
+module Distance
+  def distance(point)
+    (self.x - point.x).abs + (self.y - point.y).abs
+  end
+end
+
 class Point
+  include Distance
   attr_reader :x, :y
 
   def initialize(x, y)
     @x = x
     @y = y
   end
-
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
 end
 
 class PointWithOptStructClass < OptStruct.new(:x, :y)
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
+  include Distance
 end
 
 class PointWithOptStructModule
   include OptStruct
+  include Distance
   expect_arguments :x, :y
-
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
 end
 
 class PointWithOptStructBuilder
   include OptStruct.build(:x, :y)
-
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
+  include Distance
 end
 
 class PointOptStructRequiredKeys < OptStruct.new
   required :x, :y
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
+  include Distance
 end
 
 class PointWithStruct < Struct.new(:x, :y)
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
+  include Distance
 end
 
 class PointWithOpenStruct < OpenStruct
-  def distance(point)
-    (self.x - point.x).abs + (self.y - point.y).abs
-  end
+  include Distance
 end
 
 class PointWithActiveModel
-  attr_accessor :x, :y
   include ActiveModel::Model
-
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
-  end
+  include Distance
+  attr_accessor :x, :y
 end
 
 class PointWithHash
+  include Distance
   attr_reader :hash
 
   def initialize(**hash)
@@ -85,10 +72,6 @@ class PointWithHash
 
   def y
     hash[:y]
-  end
-
-  def distance(point)
-    (x - point.x).abs + (y - point.y).abs
   end
 end
 
